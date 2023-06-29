23883181c6a09d2a5bf20ea3e9b06bda63212bf3
Sean Behan <codebam@riseup.net>
2023-06-28 22:43:13 -0400

    fix email format

--- src/worker.ts
+++ src/worker.ts
@@ -7,11 +7,10 @@ export interface Env {
 
 const name = 'Sean';
 const reminders: any = [];
-const epoch = Math.floor(Date.now() / 1000);
 
 reminders.push('go to sleep');
 
-reminders.map((reminder: string) => reminder + '\n');
+reminders.map((reminder: string) => reminder + '\n\n');
 
 export default {
 	fetch: async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => new Response(),
@@ -19,10 +18,10 @@ export default {
 		const msg = createMimeMessage();
 		msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
 		msg.setRecipient('codebam@riseup.net');
-		msg.setSubject(epoch.toString());
+		msg.setSubject(Math.floor(Date.now() / 1000).toString());
 		msg.addMessage({
 			contentType: 'text/plain',
-			data: `Hi ${name},\nReminders:\n${reminders.toString()}`,
+			data: `Hi ${name},\n\n${reminders.toString()}`,
 		});
 		const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
 		return new Response(await env.send_emails.send(message));

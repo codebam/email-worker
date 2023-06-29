f2adfdc5f42741d41110c6a3f6e69667b160ec72
Sean Behan <codebam@riseup.net>
2023-06-28 22:28:58 -0400

    update email

--- src/worker.ts
+++ src/worker.ts
@@ -5,9 +5,10 @@ export interface Env {
 	send_emails: any;
 }
 
-const reminders: any = [];
-
 const name = 'Sean';
+const reminders: any = [];
+const epoch = Math.floor(Date.now() / 1000);
+
 reminders.push('go to sleep');
 
 reminders.map((reminder: string) => reminder + '\n');
@@ -18,10 +19,10 @@ export default {
 		const msg = createMimeMessage();
 		msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
 		msg.setRecipient('codebam@riseup.net');
-		msg.setSubject("Today's Daily Email");
+		msg.setSubject(epoch.toString());
 		msg.addMessage({
 			contentType: 'text/plain',
-			data: `Sent at epoch ${Date.now().toString()}\n` + `Hi ${name}, Reminders:\n${reminders.toString()}`,
+			data: `Hi ${name}, Reminders:\n${reminders.toString()}`,
 		});
 		const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
 		console.log(message);

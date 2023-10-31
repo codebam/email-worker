aeb6a777ece4b438730bf9f4722a2f0c2f95d41c
Sean Behan <codebam@riseup.net>
2023-10-31 12:01:59 -0400

    accept replies to add reminders

--- src/worker.ts
+++ src/worker.ts
@@ -3,27 +3,43 @@ import { createMimeMessage } from 'mimetext';
 
 export interface Env {
 	send_emails: any;
+	email_reminders: any;
 }
 
 const name = 'Sean';
-const reminders: any = [];
-
-reminders.push('go to sleep');
-
-reminders.map((reminder: string) => reminder + '\n\n');
 
 export default {
 	fetch: async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => new Response(),
 	scheduled: async (event: any, env: Env, ctx: ExecutionContext): Promise<Response> => {
+		const reminders = JSON.parse(await env.email_reminders.get('reminders')).join('\n');
 		const msg = createMimeMessage();
 		msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
 		msg.setRecipient('codebam@riseup.net');
 		msg.setSubject(new Date().toISOString());
 		msg.addMessage({
 			contentType: 'text/plain',
-			data: `Hi ${name},\n\n${reminders.toString()}`,
+			data: `Hi ${name}, here are your reminders.\n\n${reminders.toString()}`,
 		});
 		const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
 		return new Response(await env.send_emails.send(message));
+	},
+	email: async (message: any, env: Env, ctx: ExecutionContext) => {
+		const subject = message.headers.get('subject');
+		if (subject === 'clear') {
+			await env.email_reminders.put('reminders', JSON.stringify([]));
+			const msg = createMimeMessage();
+			msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
+			msg.setRecipient('codebam@riseup.net');
+			msg.setSubject(new Date().toISOString());
+			msg.addMessage({
+				contentType: 'text/plain',
+				data: 'reminders cleared',
+			});
+			const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
+			await env.send_emails.send(message);
+		} else {
+			const reminders = JSON.parse(await env.email_reminders.get('reminders'));
+			await env.email_reminders.put('reminders', JSON.stringify([...reminders, subject]));
+		}
 	},
 };

dff3c65038197c494857e858388928c5753b57e6
Sean Behan <codebam@riseup.net>
2023-10-31 12:05:59 -0400

    only send emails if there are reminders

--- src/worker.ts
+++ src/worker.ts
@@ -12,16 +12,20 @@ export default {
 	fetch: async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => new Response(),
 	scheduled: async (event: any, env: Env, ctx: ExecutionContext): Promise<Response> => {
 		const reminders = JSON.parse(await env.email_reminders.get('reminders')).join('\n');
-		const msg = createMimeMessage();
-		msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
-		msg.setRecipient('codebam@riseup.net');
-		msg.setSubject(new Date().toISOString());
-		msg.addMessage({
-			contentType: 'text/plain',
-			data: `Hi ${name}, here are your reminders.\n\n${reminders.toString()}`,
-		});
-		const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
-		return new Response(await env.send_emails.send(message));
+		if (reminders.length !== 0) {
+			const msg = createMimeMessage();
+			msg.setSender({ name: 'Cloudflare Workers', addr: 'cloudflare-workers@seanbehan.ca' });
+			msg.setRecipient('codebam@riseup.net');
+			msg.setSubject(new Date().toISOString());
+			msg.addMessage({
+				contentType: 'text/plain',
+				data: `Hi ${name}, here are your reminders.\n\n${reminders.toString()}`,
+			});
+			const message = new EmailMessage('cloudflare-workers@seanbehan.ca', 'codebam@riseup.net', msg.asRaw());
+			return new Response(await env.send_emails.send(message));
+		} else {
+			return new Response('ok');
+		}
 	},
 	email: async (message: any, env: Env, ctx: ExecutionContext) => {
 		const subject = message.headers.get('subject');
